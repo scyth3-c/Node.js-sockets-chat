c40315a5930dafabcf8fdc243ce321ef04e0ff1c
scyth3-c <kevintor2246@gmail.com>
2021-05-08 15:09:23 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -10,22 +10,17 @@ const ctl = express();
 console.log(color.cyan("iniciando todo"));
 
 //servidor > > 
-ctl.use(express.static(path.join(__dirname, 'public')));
+ctl.use(express.static(path.join(__dirname, '../public')));
 
 ctl.set('port', process.env.PORT || 3000);
 const server_conf = ctl.listen(ctl.get('port'), () => {
  console.log(color.green("puerto: %s"), ctl.get('port'));
 });
-
-
 //socket > >
 const io = socketio(server_conf);
 io.on('connection', (socket) => {
-    console.log(color.green("Nuevo Usuario Conectado"));
     nuser = io.engine.clientsCount;
     io.sockets.emit('users', nuser);
-  
-
     socket.on('chat:mensaje', (data) => {
        io.sockets.emit('respuesta',data);
     })
